14e350712f66fa575c043a45edf03a7ad529c5d5
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-03-18 19:59:53 +0300

    refactor(commitlint): add `defineConfig`

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,7 +1,35 @@
 import type { UserConfig } from '@commitlint/types';
 
-const commitLintConfig: UserConfig = {
-  extends: ['@commitlint/config-conventional'],
+/**
+ * Define an Commitlint config.
+ *
+ * @param {object} config Commitlint config.
+ *
+ * @returns Commitlint config.
+ */
+const defineConfig = (config: UserConfig): UserConfig => {
+  return config;
 };
 
-export default commitLintConfig;
+export default defineConfig({
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'build',
+        'chore',
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'spec',
+        'style',
+      ],
+    ],
+  },
+});
