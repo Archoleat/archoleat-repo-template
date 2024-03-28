c532c86ebc0a9e41fc9fe392e3fc0357ac318c77
Almanov Nikita <131481562+nikkeyl@users.noreply.github.com>
2024-03-28 17:49:51 +0300

    refactor: change export type

--- specs/index.spec.ts
+++ specs/index.spec.ts
@@ -1,10 +1,10 @@
 import { describe, expect, test } from 'vitest';
 
-import hello from '../index.ts';
+import { message } from '../index.ts';
 
 describe('Message', () => {
   test('Hello!', async () => {
-    return hello('Hello!').then((argument) => {
+    return message('Hello!').then((argument) => {
       expect(argument).equal('Hello!');
     });
   });
