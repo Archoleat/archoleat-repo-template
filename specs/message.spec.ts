5fa42212819044ce29a0948ab9b755406917f9b5
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2024-12-23 01:36:09 +0300

    refactor: change path (#481)
    Signed-off-by: nikkeyl <131481562+nikkeyl@users.noreply.github.com>

--- specs/message.spec.ts
+++ specs/message.spec.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test as spec } from 'bun:test';
 
-import { message } from '#index';
+import { message } from '#src/index.ts';
 
 describe('Send Message', async () => {
   spec('should return "Hello!"', async () => {
