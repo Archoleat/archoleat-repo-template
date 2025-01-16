889ce8da50632350883b363f008ccebe1fca36cf
nikkeyl <131481562+nikkeyl@users.noreply.github.com>
2025-01-16 23:59:05 +0300

    spec: optimize
    Signed-off-by: nikkeyl <131481562+nikkeyl@users.noreply.github.com>

--- specs/message.spec.ts
+++ specs/message.spec.ts
@@ -2,10 +2,10 @@ import { describe, expect, test as spec } from 'bun:test';
 
 import { message } from '#src/index.ts';
 
-describe('Send Message', async () => {
+describe('Send Message', () => {
   spec('should return "Hello!"', async () => {
-    await message('Hello!').then((string) => {
-      expect(string).toEqual('Hello!');
-    });
+    const result = await message('Hello!');
+
+    expect(result).toEqual('Hello!');
   });
 });
